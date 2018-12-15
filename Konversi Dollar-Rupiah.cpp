fa89122962d01c96887c1889f6f0f197b2909e02
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 15:51:56 +0700

    Add files via upload

---
+++ Konversi Dollar-Rupiah.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int dollar,rupiah,hasil;
+
+cout<<"Konversi Mata Uang Dollar-Rupiah"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"311810183"<<endl;
+cout<<"Masukan nominal"<<endl;
+cin>>rupiah;
+
+hasil=rupiah*0.00006;
+cout<<"Maka nilai dollar adalah"<<hasil;
+return 0;
+}
